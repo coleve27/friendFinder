543940741e6280045a56d0035b9eb205322b0846
Evelyn Coleman <coleve27@gmail.com>
2018-02-06 19:37:25 -0600

    fixed length error but not sure what was wrong

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,12 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+var friendData = require('../data/friends.js');
 
-var friends = require("../data/friends.js");
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+module.exports = function (app) {
 
-  module.exports = function (app) {
+	app.get('/api/friends', function(req, res){
+		res.json(friendData);
+	})
+
 
 	app.post('/api/friends', function(req, res){
 		var newFriend = req.body;
